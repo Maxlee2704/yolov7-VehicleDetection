88131e0b33c99bb431f830ea2078258fc0eee647
LAPTOP-LSKCBG6A\Admin <lhhoan2704@gmail.com>
2023-04-06 10:49:29 +0700

    code with new weight

--- detect.py
+++ detect.py
@@ -165,7 +165,7 @@ def detect(save_img=False):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--weights', nargs='+', type=str, default='yolov7.pt', help='model.pt path(s)')
+    parser.add_argument('--weights', nargs='+', type=str, default='runs/train/exp/best (1).pt', help='model.pt path(s)')
     parser.add_argument('--source', type=str, default='D:/BKU/Monhoc/222/project2/yolov7/train_data/Data/Data/6660778025474221249_045.jpg', help='source')  # file/folder, 0 for webcam
     parser.add_argument('--img-size', type=int, default=640, help='inference size (pixels)')
     parser.add_argument('--conf-thres', type=float, default=0.25, help='object confidence threshold')
